91a1545723cddb8997a282f3e1f308a5fb402f12
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-08-10 16:34:16 +0300

    gender-selection-view

--- FirebaseTutorial/Pages/Authentication/Survey/GenderSelectionView.swift
+++ FirebaseTutorial/Pages/Authentication/Survey/GenderSelectionView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct GenderSelectionView: View {
+    @State var selectedGender : Gender = .female
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Choose your gender")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(.white)
+            Spacer().frame(height: 20)
+            GenderSelectionTile(gender: "Female",selected: $selectedGender)
+            Spacer().frame(height: 20)
+            GenderSelectionTile(gender: "Male",selected: $selectedGender)
+            Spacer().frame(height: 20)
+            GenderSelectionTile(gender: "Other",selected: $selectedGender)
+            Spacer()
+            NavigationLink(destination: GenderSelectionView()) {
+                Text("Continue")
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.royalBlue)
+                    .padding()
+            }
+
+        }.padding(.horizontal,20)
+        .background(
+            Color.neroBlack.ignoresSafeArea()
+        )
     }
 }
 
 #Preview {
     GenderSelectionView()
+}
+
+struct GenderSelectionTile: View {
+    let gender: String
+    @Binding var selected: Gender
+
+    var body: some View {
+        HStack {
+            Text(gender)
+                .font(.system(size: 14, weight: .semibold))
+                .foregroundColor(gender == selected.rawValue.capitalized ? .vividOrange : .white)
+            Spacer()
+            Image(gender)
+                .resizable()
+                .scaledToFit()
+                .frame(width:40,height:40)
+        }
+        .padding(.horizontal,20)
+        .padding(.vertical,10)
+        .onTapGesture {
+            if gender == "Female" {
+                selected = .female
+            }
+            else if gender == "Male" {
+                selected = .male
+            }
+            else {
+                selected = .other
+            }
+        }
+        .background(
+            Color.textfieldBg
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
 }
