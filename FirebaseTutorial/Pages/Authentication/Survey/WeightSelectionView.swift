b62fdaa5105ef0393d67efb190562f93628f37ad
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-08-10 20:22:51 +0300

    weight-selection-view

--- FirebaseTutorial/Pages/Authentication/Survey/WeightSelectionView.swift
+++ FirebaseTutorial/Pages/Authentication/Survey/WeightSelectionView.swift
@@ -1,5 +1,5 @@
 //
-//  HeightSelectionView.swift
+//  WeightSelectionView.swift
 //  FirebaseTutorial
 //
 //  Created by Mertcan Erbaşı on 10.08.2024.
@@ -7,28 +7,28 @@
 
 import SwiftUI
 
-struct HeightSelectionView: View {
+struct WeightSelectionView: View {
     var registerModel: RegisterUserModel = RegisterUserModel.shared
-    @State private var selectedHeight: Int = 175
+    @State private var selectedWeight: Int = 70
     var body: some View {
         VStack {
-            Text("How tall are you?")
+            Text("How much do you weigh?")
                 .multilineTextAlignment(.center)
                 .font(.system(size: 20, weight: .medium))
                 .foregroundColor(.white)
             Spacer()
-            Picker(selection: $selectedHeight, label: Text("")) {
-                            ForEach(140...250, id: \.self) { height in
-                                Text("\(height)")
+            Picker(selection: $selectedWeight, label: Text("")) {
+                            ForEach(30...250, id: \.self) { weight in
+                                Text("\(weight)")
                                     .font(.system(size: 24, weight: .semibold))
                                     .foregroundColor(.white)
-                                    .tag(height)
+                                    .tag(weight)
                             }
                         }
                         .pickerStyle(WheelPickerStyle())
                         .frame(height: 200)
-                        .onChange(of: selectedHeight) { oldValue,newValue in
-                            registerModel.height = newValue
+                        .onChange(of: selectedWeight) { oldValue,newValue in
+                            registerModel.weight = newValue
                         }
             Spacer()
             NavigationLink(destination: WeightSelectionView()) {
@@ -46,5 +46,5 @@ struct HeightSelectionView: View {
 }
 
 #Preview {
-    HeightSelectionView()
+    WeightSelectionView()
 }
